a077f57feba855d9472dfda74cf819f412344d42
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-04-16 20:49:51 +0530

    First commit

---
+++ JavaBasics/src/com/cg/basicstwo/OperatorsDemo.java
@@ -0,0 +1,32 @@
+package com.cg.basicstwo;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		// Arithmetic operators are 
+		// +,-,*(multiplication),/(division),%(modulo operators outputs the reminders)
+		
+        // Assignment Operators:
+		// =, += (int x +=1 means int x =x+1), -=, *=, /= and %=. All these work the same way.
+
+		// Comparison Operators:
+        // == (equal to), != (not equal to), > (greater than), < (less than), 
+		// >= (greater than or equal to) and <= (less than or equal to)
+		
+		// Logical Operators:
+        // && (logical AND), || (logical OR) and ! (logical NOT)
+		
+		//Conditional Operator:
+        //eg
+		int toCheck=34;
+		String ifIs;
+		ifIs= (toCheck == 34)? "right": "wrong";
+		// this translates to:
+		if(toCheck==34)
+			ifIs="right";
+		else
+			ifIs="wrong";
+
+	}
+
+}
