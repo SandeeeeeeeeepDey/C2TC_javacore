3c06dbe6739e2447a0ff7b37e8482238dc90b56d
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-04-16 20:49:13 +0530

    First commit

---
+++ JavaBasics/src/com/cg/basicsone/DataTypesDemo.java
@@ -0,0 +1,21 @@
+package com.cg.basicsone;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// data types are of 2 types: primitive and non-primitive(or reference)
+		// primitive are: byte-1byte, short-2byte, int-4byte, long-8byte,
+		// float-4byte, double-8byte, 
+		// char-2byte and boolean-1byte. 
+		byte myByte=127; //-ve 128
+		short myShort=32767; //-ve 32768; negative max is one more than positive max 
+		int myInt=-2147483648;
+		long myLong= 9223372036854775807L; // Default it takes int if l or L is not mentioned, same for float.
+		float myFloat= 9999999f; // Default is double; upto 6-6 decimal digits
+		double myDouble= 999999999999999D;  //default is int; accepts upto 15 decimal digits
+		char myChar=76; // any character, alphabets in upper or lower case, or their corresponding ASCII or UNICODE value is allowed.
+		boolean myBoolean=true; // true or false
+
+	}
+
+}
