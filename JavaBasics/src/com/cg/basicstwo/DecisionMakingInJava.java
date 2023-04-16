7e740ccdf193bd906b3e6fcd8199a7349a957d2e
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-04-16 21:12:22 +0530

    first commit

---
+++ JavaBasics/src/com/cg/basicstwo/DecisionMakingInJava.java
@@ -0,0 +1,33 @@
+package com.cg.basicstwo;
+
+public class DecisionMakingInJava {
+
+	public static void main(String[] args) {
+		// There are Three ways to do decision making in Java: if-else, switch statements and forEach
+		
+		//If-else 
+		int numOne= 10;
+		int numTwo=20;
+		int numThree=30;
+		if(numOne >numTwo){
+			if(numOne >numThree)
+				System.out.println(numOne + " is largest");
+			else
+				System.out.println(numThree + " is largest");
+		}else if(numTwo>numOne)
+		{
+			if(numTwo >numThree)
+				System.out.println(numTwo + " is largest");
+			else
+				System.out.println(numThree + " is largest");
+		}else {
+			System.out.println("Both "+numOne+" and "+numTwo+" are same" );
+			if(numOne>numThree)
+				System.out.println(numOne+" and "+numTwo+" are equal and greater than "+ numThree);
+			else
+				System.out.println(numOne+" and "+numTwo+" are equal and smaller than "+ numThree);
+			
+
+	}
+
+}
