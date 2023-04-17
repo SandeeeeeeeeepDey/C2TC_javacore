1a6f75c3d729a96b89226f882d5898ece0202e09
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-04-17 10:38:31 +0530

    first commit

---
+++ JavaBasics/src/com/cg/basicstwo/LoopsWhileLoop.java
@@ -0,0 +1,20 @@
+package com.cg.basicstwo;
+
+public class LoopsWhileLoop {
+
+	public static void main(String[] args) {
+		// While loop compares an already initiated value, if true, it performs the task
+		// in the task, we have to mention a change in the value, or it loops to infinity
+		// format of while loop: while(comparison){task, incrementation}
+		// example:
+		int i=0;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		// prints all numbers in between 0 to 10
+		// loop iterates only if comparison is true
+
+	}
+
+}
