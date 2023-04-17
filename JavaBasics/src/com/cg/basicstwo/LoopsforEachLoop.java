9c4750f3a6067b6c6c087f73396ab467ab08c722
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-04-17 10:30:42 +0530

    first commit

---
+++ JavaBasics/src/com/cg/basicstwo/LoopsforEachLoop.java
@@ -0,0 +1,17 @@
+package com.cg.basicstwo;
+
+public class LoopsforEachLoop {
+
+	public static void main(String[] args) {
+		// forEach loops
+		// this loop iterates for each of the elements in a given range of elements from index one to length-1
+		// example:
+		String[] animals= {"Hamster", "Dog", "Cat", "Whale", "Platypus"};
+		for(String animal:animals){
+			System.out.println(animal);
+		}
+		// in this loop it goes through all the elements in the array animals and prints each one.
+
+	}
+
+}
