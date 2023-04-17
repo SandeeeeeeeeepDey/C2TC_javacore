a5d8c1547a3511d7a998b7ab72d047da803458c9
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-04-17 10:22:31 +0530

    first commit

---
+++ JavaBasics/src/com/cg/basicstwo/LoopsForLoop.java
@@ -0,0 +1,22 @@
+package com.cg.basicstwo;
+
+public class LoopsForLoop {
+
+	public static void main(String[] args) {
+		// Loops in Java are of three types for loop, while loop, do-while loop and forEsach loop
+		
+		// for loops:
+		// form of for loop: for(initialization; comparison; incrementation/decrementation){ start}
+		// example:
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		} // this prints all integers in between 0 to 10
+		
+		// first it initializes the datatype, then compares with the condition, if true,
+		// it performs the task and does the incrimentation.
+		// for all next iterations, it only compares the new value, performs the task, increments further 
+		// and so on...
+
+	}
+
+}
