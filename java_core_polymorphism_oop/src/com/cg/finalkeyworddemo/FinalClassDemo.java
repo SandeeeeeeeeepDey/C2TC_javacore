63afb8a4a25311139bb06656b5d1d99e9cb89e7b
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-05-04 22:40:36 +0530

    first commit

--- java_core_polymorphism_oop/src/com/cg/finalkeyworddemo/FinalClassDemo.java
+++ java_core_polymorphism_oop/src/com/cg/finalkeyworddemo/FinalClassDemo.java
@@ -1,7 +1,7 @@
 // Program to demonstrate Final class
 package com.cg.finalkeyworddemo;
 // Parent class
-public final class FinalClassDemo { // We cant inherit a final class in the child class used to demonstrate.
+public final class FinalClassDemo { // We cant inherit a final class in the child class used to demonstrate. 
 	// final variable
 	private final String NAME= "Bat Man";
 	public final void display() {
