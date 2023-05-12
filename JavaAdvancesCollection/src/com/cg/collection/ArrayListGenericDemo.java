1f016c523a9d53ef90db56cda8f970caf79b05cf
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-05-12 16:42:39 +0530

    first commit

---
+++ JavaAdvancesCollection/src/com/cg/collection/ArrayListGenericDemo.java
@@ -0,0 +1,28 @@
+// Program to demonstrate generic ArrayList.
+package com.cg.collection;
+import java.util.*;
+// Duplication, order of output
+public class ArrayListGenericDemo {
+
+	public static void main(String[] args) {
+		// ArrayList can have any type of object as element, as demonstrated in the CollectionDemo class.
+		// If we want an Array without a fixed size but has homogeneous elements, we can write the ArrayList as generic.
+		// it is done by adding a datatype within diamond bracket as type, similar to arrays. and an empty diamond bracket in the initialization side.
+		ArrayList<Integer> nums=new ArrayList<>();
+		nums.add(3);
+		nums.add(6);
+		nums.add(4);
+		nums.add(1);
+		nums.add(49);
+		nums.add(0,6); // Accepts Duplication.
+		nums.add(null); // Accepts null, but cant perform methods like sort.
+		System.out.println(nums); // Order of output is insertion order.
+		nums.remove(null); // Remove a value
+		nums.remove(3); // When we put an integer in remove method, it take the integer to be an index value instead of value.
+		Collections.sort(nums); // Sort in ascending order.
+		System.out.println(nums);
+		
+		
+	}
+
+}
---
+++ JavaAdvancesCollection/src/com/cg/collection/CollectionDemo.java
@@ -0,0 +1,96 @@
+// This program is to demonstrate implementable classes from Collection: ArrayList.
+package com.cg.collection;
+import java.util.*;
+public class CollectionDemo {
+		// Arrays have limitations such as fixed size and homogeneous elements. 
+		// Collection is in interface that solves this problem.
+	
+	public static void main(String[] args) {
+		// Collection implementable classes can have all primitive, non-primitive type data and also user defined class type inputs.
+		Boys b=new Boys(180,76,"Bat Man",true), bOne=new Boys(180,76,"Super Man",true);
+		Girls g=new Girls(180,76,"Bat Woman",true), gOne=new Girls(180,76,"Super Woman",true);
+		Collection list=new ArrayList();
+		list.add("abc");
+		list.add(8);
+		list.add(3.14);
+		list.add(false);
+		list.add(gOne);
+		list.add(g);
+		list.add(b);
+		list.add(bOne);
+		System.out.println(list);
+		System.out.println(list.contains(gOne));
+		System.out.println(list.contains("Bat Man"));
+	
+System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+	// There are 4 ways commonly used to iterate through an array list:
+		// for loop:
+			for(int i=0;i<list.size(); i++) { // Since the length is not fixed, 
+				System.out.println(((ArrayList) list).get(i)); // There is no get method in Collection Interface, so we have to type-cast it to ArrayList.
+	}
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+		// for-each loop:
+			for(Object obj:list) 
+				System.out.println(obj);
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+		// Iterator loop:
+			Iterator itr=list.iterator();
+			while(itr.hasNext()) 
+				System.out.println(itr.next());
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+		// ListIterator:
+			ListIterator Litr=((ArrayList) list).listIterator(); // ListIterator is a subclass of ArrayList
+			
+			while(Litr.hasNext()) 
+				System.out.println(Litr.next());
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+			while(Litr.hasPrevious()) 
+				System.out.println(Litr.previous());
+	
+
+	}
+}
+// Class for user defined Class type Object.
+class Boys{
+	int height;
+	int weight;
+	String name;
+	boolean read;
+	Boys(int height, int weight, String name, boolean read){
+		this.height=height;
+		this.weight=weight;
+		this.name=name;
+		this.read=read;
+	}
+	@Override
+	public String toString() {
+		return "Boys [height=" + height + ", weight=" + weight + ", name=" + name + ", read=" + read + "]";
+	}
+	
+// One more class for user defined Class type Object.
+}
+class Girls{
+	int height;
+	int weight;
+	String name;
+	boolean read;
+	Girls(int height, int weight, String name, boolean read){
+		this.height=height;
+		this.weight=weight;
+		this.name=name;
+		this.read=read;
+	}
+	@Override
+	public String toString() {
+		return "Boys [height=" + height + ", weight=" + weight + ", name=" + name + ", read=" + read + "]";
+	}
+}
