1866719fb4dadb365ab8453d7536eb3c0b07b8ab
SandeeeeeeeeepDey <sandytec11@gmail.com>
2023-05-31 16:20:52 +0530

    first commit

---
+++ JavaAdvancedGenericConcept/src/com/cg/genericconstructor/GenericConstructorDemo.java
@@ -0,0 +1,10 @@
+package com.cg.genericconstructor;
+
+public class GenericConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaAdvancedGenericConcept/src/com/cg/genericupperbounds/GenericUpperBoundConcept.java
@@ -0,0 +1,10 @@
+package com.cg.genericupperbounds;
+
+public class GenericUpperBoundConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
